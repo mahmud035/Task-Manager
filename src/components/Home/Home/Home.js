4052852164d1d1ff0d1bcadfcfe1f0b5abb948b6
mahmud035 <mahamudulhasan4148@gmail.com>
2022-12-27 19:46:07 +0600

    added signIn and register button into home page

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
 import AddTask from '../../AddTask/AddTask';
 import './Home.css';
 
 const Home = () => {
+  const { user } = useContext(AuthContext);
   return (
     <div>
       <div className="container">
-        <h1>Home Component</h1>
+        {!user?.email && (
+          <div className="d-flex flex-column justify-content-center align-items-center gap-3 vh-100 text-center">
+            <h2>Please SignIn or Register for Managing Your Task</h2>
+            <div className="d-flex gap-3">
+              <Link to="/signin">
+                <Button variant="success" className="btn-sign-in">
+                  SignIn
+                </Button>
+              </Link>
+              <Link to="/register">
+                <Button className="btn-register">Register</Button>
+              </Link>
+            </div>
+          </div>
+        )}
         {/* <AddTask></AddTask> */}
       </div>
     </div>
